2175d2d497190acc973e7aa76b2e1e484a180f05
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-07-04 08:21:52 -0700

    pricing updates

--- app/components/side-accordion.js
+++ app/components/side-accordion.js
@@ -14,21 +14,29 @@ const SideAccordion = () => {
         {
             title: "BASIC PLAN",
             content: [
-                "If your needs do not require frequent updates, the BASIC PLAN is your best option.",
-                "A 3-5 page website is generally around $1300 + a monthly Hosting / Retainer fee.",
-                "A single page website would be signifcantly less of course.",
-                "The Hosting / Retainer fee includes hosting the website for $20 a month and ensures our availability for content updates and further enhancements.",
-                "If you do need to update the copy and images on the website, it is $30 an hour with a 1 hour minimum. Generally all basic updates can be done within 1 hour."
+                // "If your needs do not require frequent updates, the BASIC PLAN is your best option.",
+                // "A 3-5 page website is generally around $1300 + a monthly Hosting / Retainer fee.",
+                // "A single page website would be signifcantly less of course.",
+                // "The Hosting / Retainer fee includes hosting the website for $20 a month and ensures our availability for content updates and further enhancements.",
+                // "If you do need to update the copy and images on the website, it is $30 an hour with a 1 hour minimum. Generally all basic updates can be done within 1 hour."
+                "If you need a simple static website and do not require frequent updates, the BASIC PLAN is an excellent choice. The cost varies based on several factors, but typically falls around the $1500 zone.",
+                "There is a Hosting/Retainer fee of $50 a month which covers the cost of hosting your website and guarantees our support for content updates and further improvements.",
+                "For updates to copy and images on the website, we offer flexible hourly rates with a minimum time requirement of one hour. Most basic updates are efficiently handled within this timeframe.",
+                "A BASIC PLAN website is a cost-effective simplified solution to establish your online presence.",
             ]
         },
         {
             title: "UPDATEABLE PLAN",
             content: [
+                // "If you would like full control over the content of your website and would like to change copy and images whenever you like, the UPDATABLE PLAN is best.",
+                // "We would build your site in WordPress, a globally renowned Content Management System (CMS) known for its versatility and ease of use. With WordPress, you'll have access to an intuitive dashboard to update content whenever you want.",
+                // "A 3-5 page website is generally around $1300 + a monthly Hosting / Retainer fee.",
+                // "A single page website would be signifcantly less.",
+                // "The Hosting / Retainer fee includes hosting the website for $30 a month and ensures our availability for any questions regarding the Content Management System."
                 "If you would like full control over the content of your website and would like to change copy and images whenever you like, the UPDATABLE PLAN is best.",
                 "We would build your site in WordPress, a globally renowned Content Management System (CMS) known for its versatility and ease of use. With WordPress, you'll have access to an intuitive dashboard to update content whenever you want.",
-                "A 3-5 page website is generally around $1300 + a monthly Hosting / Retainer fee.",
-                "A single page website would be signifcantly less.",
-                "The Hosting / Retainer fee includes hosting the website for $30 a month and ensures our availability for any questions regarding the Content Management System."
+                "A 3+ page website is generally around $3000 + a monthly Hosting / Retainer fee. A single page website would be significantly less.",
+                "There is a $50 monthly Hosting/Retainer fee that includes hosting the website and guarantees our availability for any CMS-related inquiries. Alternatively, you are welcome to self-host, in which case we would only charge the retainer fee for support. If you prefer not to manage any content updates at all and simply want to offload all website management, we offer flexible hourly rates to handle everything for you."
             ]
         },
         {
